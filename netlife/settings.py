b1bf5e42ef8750d24c499fc870c82761f97cb0da
Danny Ibishev <danny.ibishev@cognitoiq.com>
2018-06-20 14:40:21 +0100

    main html structure set up

--- netlife/settings.py
+++ netlife/settings.py
@@ -15,7 +15,10 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TEMPLATE_DIR = os.path.join(BASE_DIR, 'webui/templates/',)
-print("Using TEMPLATE_DIR = {0}".format(TEMPLATE_DIR))
+BOWER_COMPONENTS_ROOT = os.path.join(BASE_DIR, 'bower_components/',)
+APP_CSS_PATH = os.path.join(BASE_DIR, 'webui/templates/css/',)
+BOWER_PATH = '/usr/local/bin/bower'
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
@@ -123,12 +126,19 @@ USE_TZ = False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    BOWER_COMPONENTS_ROOT,
+    APP_CSS_PATH,
+)
 
 STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
     'djangobower.finders.BowerFinder',
 )
 
 BOWER_INSTALLED_APPS = (
     'bootstrap',
 )
+
+STATIC_URL = '/static/'
